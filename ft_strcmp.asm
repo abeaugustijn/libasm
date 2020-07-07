9095ebfa5262de4f52a93c88d3efd3ebbe7e0c9c
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-07-07 21:26:20 +0200

    Fix: strcmp

--- ft_strcmp.asm
+++ ft_strcmp.asm
@@ -14,10 +14,10 @@ loop:
 	mov r8b, [rcx + rax]	; Get the char of the first string into r8b
 	mov r9b, [rdx + rax]	; Get the char of the second string into r9b
 	inc rax
-	cmp r8b, r9b			; Compare the two
-	jne ret					; Return if they are not equal
 	cmp r8b, 0x0			; Check if the strings have ended
 	je ret
+	cmp r8b, r9b			; Compare the two
+	jne ret					; Return if they are not equal
 	jmp loop
 
 ret:
@@ -25,6 +25,8 @@ ret:
 							; string into rax.
 	and rax, 0xFF			; Make sure that only the lowest byte is set, as we
 							; were just using those.
+	and r9, 0xFF			; Use only one byte of the char from the second
+							; string
 	sub rax, r9				; Substract the value of the char in the second
 							; string from the one we had.
 	ret
--- test/ft_strcmp.c
+++ test/ft_strcmp.c
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:    :+:            */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaugusti <aaugusti@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/20 22:14:15 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/03/20 22:59:49 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/07/07 21:13:02 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libasm.h"
-#include <string.h>
+#include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 static void ft_strcmp_test_one(char *str1, char *str2)
 {
 	int a = ft_strcmp(str1, str2);
 	int b = strcmp(str1, str2);
 	printf("\texpected: %d\tgot: %d\n", b, a);
+	assert(a == b);
 }
 
 void	ft_strcmp_test(void)
