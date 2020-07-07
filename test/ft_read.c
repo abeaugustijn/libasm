7505dd8e8fd43fa6b5b266c3019e00863e848d51
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-07-07 21:27:11 +0200

    Fix: update read test

--- test/ft_read.c
+++ test/ft_read.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_read.c                                          :+:      :+:    :+:   */
+/*   ft_read.c                                          :+:    :+:            */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaugusti <aaugusti@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/23 10:03:11 by aaugusti          #+#    #+#             */
-/*   Updated: 2020/03/23 10:10:00 by aaugusti         ###   ########.fr       */
+/*   Updated: 2020/07/07 21:07:30 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,17 @@ void	ft_read_test(void)
 	char	buf2[BUFSIZE];
 	int		fd1;
 	int		fd2;
+	int		read1_ret;
+	int 	read2_ret;
 
 	bzero(buf1, BUFSIZE);
 	bzero(buf2, BUFSIZE);
 	fd1 = open(FILENAME, O_RDONLY);
 	fd2 = open(FILENAME, O_RDONLY);
 	printf("\nTEST: ft_read \n");
-	assert(read(fd1, buf1, BUFSIZE) == ft_read(fd2, buf2, BUFSIZE));
-	printf("buf1: %s\n", buf1);
-	printf("buf2: %s\n", buf2);
-	if (ft_strcmp(buf1, buf2))
+	read1_ret = read(fd1, buf1, BUFSIZE);
+	read2_ret = ft_read(fd2, buf2, BUFSIZE);
+	assert(read1_ret == read2_ret);
+	if (strncmp(buf1, buf2, read1_ret))
 		printf("ft_read_test failed\n");
 }
